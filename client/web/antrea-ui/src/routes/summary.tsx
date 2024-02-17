39f43792607180070a40969776d4113092a62f33
Roopesh Saravanan <roopeshsaravanan.dev@gmail.com>
2024-02-17 22:22:42 +0530

    Add a card to the summary page
    Added a card to the summary page (summary.tsx)

--- client/web/antrea-ui/src/routes/summary.tsx
+++ client/web/antrea-ui/src/routes/summary.tsx
@@ -162,6 +162,17 @@ export default function Summary() {
                 <WaitForAPIResource ready={agentInfos !== undefined} text="Loading Agents Information">
                     <ComponentSummary title="Agents" data={agentInfos!} propertyNames={agentProperties} getProperties={agentPropertyValues} />
                 </WaitForAPIResource>
+                <CdsCard>
+                    <div cds-layout="vertical gap:md">
+                        <div cds-text="section" cds-layout="p-y:sm">
+                            Hello World
+                        </div>
+                        <CdsDivider cds-card-remove-margin></CdsDivider>
+                        <div cds-layout="p-y:sm">
+                            <p cds-text="body">My Github ID is roopeshsn</p>
+                        </div>
+                    </div>
+                </CdsCard>
             </div>
         </main>
     );
